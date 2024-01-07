c2a0079ded541ca989b4f10d73f2fdd13caabc93
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2024-01-07 13:34:48 +0900

    Todo store만들기, 이제 이걸 가지고 UI를 만든다

--- src/TodoStore.ts
+++ src/TodoStore.ts
@@ -1,3 +1,5 @@
+import { action, computed, makeObservable, observable } from "mobx";
+
 interface TodoItem{
   id: number;
   title: string;
@@ -7,6 +9,45 @@ interface TodoItem{
 export default class TodoStore {
   todos:TodoItem[] = [];
   constructor(){
-    makeObservable()
+    makeObservable(this, {
+      todos: observable,
+      addTodo: action,
+      toggleTodo: action,
+      status: computed
+    })
+  }
+
+  addTodo = (title:string) => {
+    const item: TodoItem = {
+      id: getId(),
+      title,
+      completed: false
+    }
+    this.todos.push(item);
+  }
+
+  toggleTodo = (id:number) => {
+    const index = this.todos.findIndex((item) => item.id === id);
+    if(index !== -1) {
+      this.todos[index].completed = !this.todos[index].completed;
+    }
+  }
+
+  status = () {
+    let completed = 0, remaining = 0;
+    this.todos.forEach((todo) => {
+      if(todo.completed) {
+        completed++;
+      } else {
+        remaining++;
+      }
+    })
+
+    return{completed, remaining}
   }
 }
+
+let id = 0;
+function getId(){
+  return id++;
+} 
